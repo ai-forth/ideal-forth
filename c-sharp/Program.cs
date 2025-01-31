6c73ae930b8e5abc1f1278dc3bb62ce944bab7dc
cartheur <cartheur@pm.me>
2025-01-31 14:11:56 +0100

    Add program tempate

--- c-sharp/Program.cs
+++ c-sharp/Program.cs
@@ -1,4 +1,19 @@
-﻿
+﻿// Begin program
 
+Console.WriteLine("An example of the simplest task-runner fast-compute in c-sharp.");
+Console.WriteLine("Computing operations with a vector struct.");
 
-Console.WriteLine("Hello, World!");
+// Computations to perform.
+var sum = Vector.x + Vector.y;
+
+// Outputs to print.
+Console.WriteLine("Print the addition of two vectors.: " + sum);
+
+// Structures and operations for this program.
+struct Vector
+{
+    public static int x = 5;
+    public static int y = 4;
+}
+
+// End program
