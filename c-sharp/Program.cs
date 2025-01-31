6fd7f0055440fdca4e2510d1eed969040cdb571b
cartheur <cartheur@pm.me>
2025-01-31 15:26:52 +0100

    Add diff

--- c-sharp/Program.cs
+++ c-sharp/Program.cs
@@ -9,10 +9,13 @@ var vec02 = new Vector(8, 9, 7);
 var sum = vec01 + vec02;
 Console.WriteLine("1 - Vector(5, 4, 3)");
 Console.WriteLine("2 - Vector(8, 9, 7)");
-
-// Outputs to print.
 Console.WriteLine("The sum of these vectors.: " + sum.V);
-Console.ReadLine();
+
+var diff = vec01 - vec02;
+Console.WriteLine("1 - Vector(5, 4, 3)");
+Console.WriteLine("2 - Vector(8, 9, 7)");
+Console.WriteLine("The diff of these vectors.: " + diff.V);
+//Console.ReadLine();
 
 // Structures and operations for this program.
 struct Vector
@@ -43,6 +46,10 @@ struct Vector
     public static Vector operator +(Vector v1, Vector v2)
     {
         return new Vector(v1.X + v2.X + v1.Y + v2.Y + v1.Z + v2.Z);
+    }
+    public static Vector operator -(Vector v1, Vector v2)
+    {
+        return new Vector((v1.X - v2.X) - (v1.Y + v2.Y) - (v1.Z + v2.Z));
     }
 }
 
