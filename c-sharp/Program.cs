709d2811dec9ad3fda8df56fec91ad7c38fff84a
cartheur <cartheur@pm.me>
2025-02-01 10:41:38 +0100

    Add Joly 1905 images

--- c-sharp/Program.cs
+++ c-sharp/Program.cs
@@ -49,5 +49,25 @@ struct Vector
         return new Vector((v1.X - v2.X) - (v1.Y + v2.Y) - (v1.Z + v2.Z));
     }
 }
+struct Complex
+{
+    public double Real { get; set; }
+    public double Imaginary { get; set; }
+
+    public Complex(double real, double imaginary)
+    {
+        Real = real;
+        Imaginary = imaginary;
+    }
+
+    public static Complex operator +(Complex c1, Complex c2)
+    {
+        return new Complex(c1.Real + c2.Real, c1.Imaginary + c2.Imaginary);
+    }
+    public static Complex operator -(Complex c1, Complex c2)
+    {
+        return new Complex((c1.Real - c2.Real), (c1.Imaginary + c2.Imaginary));
+    }
+}
 
 // End program
